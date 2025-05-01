f42abb23584372002a33f155753815a36c8d7226
Your Name <your.email@example.com>
2025-05-02 00:24:26 +0530

    Update deployment configurations to fix 404 error

--- Ochi-main/vite.config.js
+++ Ochi-main/vite.config.js
@@ -4,10 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 })
